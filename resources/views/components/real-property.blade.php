3440ee0ef218ceb7dcf19bd016ddd7aa4fb1e15b
germanusK <germanuskeming@gmail.com>
2023-08-18 10:43:21 +0100

    mobile friendly updates

--- resources/views/components/real-property.blade.php
+++ resources/views/components/real-property.blade.php
@@ -1,14 +1,18 @@
 
 <div class="w-full min-h-screen flex flex-wrap py-16 mt-32 justify-evenly">
-    <div class="w-full min-h-screen z-20 bg-slate-900">
+    <div class="w-full z-20 bg-slate-900 py-0">
         <div class="w-full pt-8">
             <div class="text-center w-full py-1 text-white text-2xl font-semibold">Trending/latest property</div>
-            <div class="grid md:grid-cols-3 lg:grid-cols-4 items-center align-middle place-content-stretch max-h-screen overflow-x-scroll no-scrollbar">
-                @if(count($trending)>0)
-                    @foreach($trending as $value)
-                    <x-generic-item2 :data="$value"/>
-                    @endforeach
-                @endif
+            <div class=" w-full whitespace-nowrap overflow-x-scroll no-scrollbar">
+                <div class="w-fit md:w-full flex md:flex-wrap items-center align-middle place-content-stretch h-fit">
+                    @if(count($trending)>0)
+                        @foreach($trending as $value)
+                            <div class="w-84 whitespace-normal">
+                                <x-generic-item2 :data="$value"/>
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
             </div>
         </div>
     </div>
