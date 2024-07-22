da2b2e34e9bde8a28047cc144229af8d43da05ae
germanusK <germanuskeming@gmail.com>
2024-07-22 21:42:44 +0100

    services description replaced by caption on homepage

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -116,9 +116,6 @@ class Controller extends BaseController
         $data = ['name'=>$request->name, 'description'=>$request->description, 'parent_id'=>$request->parent_id??null];
         if(($icon_file = $request->file('image')) != null){
             $path = asset('uploads/category_images');
-            if(!file_exists(public_path('uploads/category_images'))){
-                mkdir(public_path('uploads/category_images'));
-            }
             $fname = 'category_'.random_int(1000000, 9999999).'_'.time().'.'.$icon_file->getClientOriginalExtension();
             $icon_file->move($path, $fname);
             $data['image'] = $path.'/'.$fname;
@@ -145,9 +142,6 @@ class Controller extends BaseController
         $data = ['name'=>$request->name, 'description'=>$request->description, 'parent_id'=>$request->parent_id];
         if(($icon_file = $request->file('image')) != null){
             $path = asset('uploads/category_images');
-            if(!file_exists(public_path('uploads/category_images'))){
-                mkdir(public_path('uploads/category_images'));
-            }
             $fname = 'category_'.random_int(1000000, 9999999).'_'.time().'.'.$icon_file->getClientOriginalExtension();
             $icon_file->move($path, $fname);
             $data['image'] = $path.'/'.$fname;
